be2703a4d35f78eee93c7a7ce087483e12241b1d
Sparsh Goyal <sparshg05@gmail.com>
2022-01-21 18:56:50 +0530

    Second change

--- src/api.js
+++ src/api.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const serverless = require("serverless-http");
+const data = require('../course.json');
+const cors = require("cors");
 
 const app = express();
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json({
-    hello: "hi!"
-  });
+app.use(cors());
+
+router.get("/data", (req, res) => {
+  // res.json({ hello: "hi!" });
+  res.send(data);
 });
 
 app.use(`/.netlify/functions/api`, router);
